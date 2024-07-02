34ad67684abd7d1884fc6a1bc6a86cb3bc60e2ea
Vaibhav <madaanvaibhav001@gmail.com>
2024-07-02 19:42:58 +0530

    made the bento grid and added public files may be changed later

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+import Grid from '@/components/Grid'
 import Hero from '@/components/Hero'
+import { FloatingNav } from '@/components/ui/floatingNavbar'
 import React from 'react'
 
 export default function page() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        page 
+         
+        {/* <FloatingNav navItems={navItems} /> */}
         <Hero/>
+        <Grid/>
       </div>
     </main>
   )
